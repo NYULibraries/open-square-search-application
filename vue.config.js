9228a7a9a664241a11c9140ecced69aeb1712b08
David <da70@nyu.edu>
2019-02-22 18:18:50 -0500

    vue.config.js: initial commit

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    baseUrl             : '/search/',
+    outputDir           : 'dist',
+    assetsDir           : 'assets',
+    indexPath           : 'index.html',
+    filenameHashing     : true,
+    lintOnSave          : 'error',
+    runtimeCompiler     : false,
+    productionSourceMap : true,
+    css                 : {
+        sourceMap : true,
+    },
+};
