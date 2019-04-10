f39d3c03459d80a72a73b5b577ad4b64ba60f1f8
David <da70@nyu.edu>
2019-04-10 11:34:37 -0400

    ResultsPaneSearchResults.metadata: replace element method with $() call using ISBN-based parent element selector

--- tests/browser/pageobjects/classes/ResultsPaneSearchResults.js
+++ tests/browser/pageobjects/classes/ResultsPaneSearchResults.js
@@ -14,18 +14,21 @@ class ResultsPaneSearchResults {
         let books   = [];
 
         results.forEach( ( result ) => {
+            const id = result.getAttribute( 'id' );
+            const parentElementSelector = `div[ id = "${ id }" ]`;
+
             let book = {};
 
-            book.author          = innerHtml( result.findElement( '.author' ).getHTML() );
-            book.description     = innerHtml( result.findElement( '.description' ).getHTML() );
-            book.isbn            = innerHtml( result.getAttribute( 'id' ) );
-            book.publicationDate = innerHtml( result.findElement( '.pubdate' ).getHTML() )
+            book.author          = innerHtml( $( `${ parentElementSelector } .author` ).getHTML() );
+            book.description     = innerHtml( $( `${ parentElementSelector } .description` ).getHTML() );
+            book.isbn            = id;
+            book.publicationDate = innerHtml( $( `${ parentElementSelector } .pubdate` ).getHTML() )
                 .replace( /<span>/g, '' )
                 .replace( /<\/span>/g, '' )
                 .replace( /Published: /, '' );
-            book.subtitle        = innerHtml( result.findElement( '.book-subtitle' ).getHTML() );
-            book.thumbnail       = new URL( result.findElement( '.thumb img' ).getAttribute( 'src' ) ).pathname;
-            book.title           = innerHtml( result.findElement( '.book-title' ).getHTML() );
+            book.subtitle        = innerHtml( $( `${ parentElementSelector } .book-subtitle` ).getHTML() );
+            book.thumbnail       = new URL( $( `${ parentElementSelector } .thumb img` ).getAttribute( 'src' ) ).pathname;
+            book.title           = innerHtml( $( `${ parentElementSelector } .book-title` ).getHTML() );
 
             books.push( book );
         } );
